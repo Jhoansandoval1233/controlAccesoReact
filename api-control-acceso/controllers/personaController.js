bfa88bf272289070be58f07bdba5fc5fcad18bd2
Jhoans Stiven Sandoval Londoño <sandovaljhoan260@gmail.com>
2025-05-27 19:48:26 -0500

    Ajustes de colores oficales sena, ajustes para responsive e intertaz

--- api-control-acceso/controllers/personaController.js
+++ api-control-acceso/controllers/personaController.js
@@ -11,6 +11,8 @@ exports.getAll = (req, res) => {
     });
 };
 
+
+
 exports.getById = (req, res) => {
     const id = req.params.id;
     Persona.getById(id, (err, results) => {
@@ -18,6 +20,14 @@ exports.getById = (req, res) => {
         if (results.length === 0) return res.status(404).json({ mensaje: 'Persona no encontrada' });
         res.json(results[0]);
     });
+};
+exports.buscarPorDocumento = (req, res) => {
+  const documento = req.params.documento;
+  Persona.getByNumeroDocumento(documento, (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    if (results.length === 0) return res.status(404).json({ mensaje: 'Persona no encontrada' });
+    res.json(results[0]);
+  });
 };
 
 exports.create = (req, res) => {
@@ -130,4 +140,8 @@ exports.delete = (req, res) => {
         }
         res.json({ mensaje: 'Persona eliminada' });
     });
+};
+// Exportar todas las funciones que usas en este controlador
+module.exports = {
+ buscarPorDocumento, 
 };
